7a57cbb7e8a9c9fb2adbfad13b009c3f81467744
dev1 <you@example.com>
2020-08-30 10:35:18 +0900

    change game delete logic

--- models/gamedef.js
+++ models/gamedef.js
@@ -1,47 +1,50 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
-const logger = require('../middleware/logger');
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const logger = require("../middleware/logger");
 
 const Gamedef = mongoose.model(
-    'Gamedef', 
-    new mongoose.Schema({
-        name: {
-            type: String,
-            required: true,
-            minlength: 3,
-            maxlength: 50
-        },
-        description: {
-            type: String,
-            required: true,
-            minlength: 5,
-            maxlength: 255
-        },
-        author: {
-            type: String,
-            required: true,
-            minlength: 3,
-            maxlength: 50,
-        },
-        status: {
-            type: Array,
-            of: String,
-            required: true
-        },
-        createdDateTime: { type: Date, default: Date.now },
-        updatedDateTime: { type: Date, default: Date.now }
-    })
+  "Gamedef",
+  new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+      },
+      description: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+      },
+      author: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+      },
+      status: {
+        type: Array,
+        of: String,
+        required: true,
+      },
+    },
+    { timestamps: true }
+  )
 );
 
 function validate(gamedef) {
-    const schema = {
-        name: Joi.string().min(3).max(50).required(),
-        description: Joi.string().min(5).max(255).required(),
-        author: Joi.string().min(3).max(50).required(),
-        status: Joi.array().items(Joi.string().min(3).max(50).required()).required(),
-    }
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    description: Joi.string().min(5).max(255).required(),
+    author: Joi.string().min(3).max(50).required(),
+    status: Joi.array()
+      .items(Joi.string().min(3).max(50).required())
+      .required(),
+  };
 
-    return Joi.validate(gamedef, schema);
+  return Joi.validate(gamedef, schema);
 }
 
-module.exports = { Gamedef, validate }
+module.exports = { Gamedef, validate };
--- routes/gamedefs.js
+++ routes/gamedefs.js
@@ -1,64 +1,77 @@
-const { Gamedef, validate } = require('../models/gamedef');
-const express = require('express');
+const { Gamedef, validate } = require("../models/gamedef");
+const { Gameevent } = require("../models/gameevent");
+const express = require("express");
 
 const router = express.Router();
 
 // get all
-router.get('/', async (req, res) => {
-    const gamedefs = await Gamedef.find();
-    res.send(gamedefs);
+router.get("/", async (req, res) => {
+  const gamedefs = await Gamedef.find();
+  res.send(gamedefs);
 });
 
 // get 1
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    const gamedef = await Gamedef.findById(id);
-    if(!gamedef) return res.status(404).send({ error: `No game def with the id ${id}` })
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const gamedef = await Gamedef.findById(id);
+  if (!gamedef)
+    return res.status(404).send({ error: `No game def with the id ${id}` });
 
-    res.send(gamedef);
+  res.send(gamedef);
 });
 
 // create 1
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if(error) return res.status(400).send({error : error.details[0].message});
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send({ error: error.details[0].message });
 
-    const gamedef = new Gamedef({
-        name: req.body.name,
-        description: req.body.description,
-        author: req.body.author,
-        status: req.body.status,
-    });
-    await gamedef.save();
+  const gamedef = new Gamedef({
+    name: req.body.name,
+    description: req.body.description,
+    author: req.body.author,
+    status: req.body.status,
+  });
+  await gamedef.save();
 
-    res.send(gamedef);
+  res.send(gamedef);
 });
 
 // update 1
-router.put('/:id', async (req, res) => {
-    const { error } = validate(req.body);
-    if(error) return res.status(400).send({error : error.details[0].message});
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send({ error: error.details[0].message });
 
-    const { id } = req.params;
-    const gamedef = await Gamedef.findByIdAndUpdate(id, {
-        name: req.body.name,
-        description: req.body.description,
-        author: req.body.author,
-        status: req.body.status,
-        updatedDateTime: new Date()
-    }, {new: true});
-    if(!gamedef) return res.status(404).send({ error: `No game def with the id ${id}` })
-    
-    res.send(gamedef);
+  const { id } = req.params;
+  const gamedef = await Gamedef.findByIdAndUpdate(
+    id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      author: req.body.author,
+      status: req.body.status,
+    },
+    { new: true }
+  );
+  if (!gamedef)
+    return res.status(404).send({ error: `No game def with the id ${id}` });
+
+  res.send(gamedef);
 });
 
 // delete 1
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    const gamedef = await Gamedef.findByIdAndRemove(id);
-    if(!gamedef) return res.status(404).send({ error: `No game def with the id ${id}` })
-    
-    res.send(gamedef);
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const gamedef = await Gamedef.findByIdAndRemove(id);
+  if (!gamedef)
+    return res.status(404).send({ error: `No game def with the id ${id}` });
+
+  // TODO : add transaction
+  const gameevents = await Gameevent.find({ gamedef: id });
+  gameevents.forEach((ge) => {
+    ge.remove();
+  });
+
+  res.send(gamedef);
 });
 
 module.exports = router;
